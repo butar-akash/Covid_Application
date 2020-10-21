6078110535059982fbc3c25124edc3d730668209
ishit <ishitvaverma1gmail.com>
2020-10-21 18:42:53 +0530

    API Call Commit

--- app/src/main/java/com/example/covid_19_application/MainActivity.java
+++ app/src/main/java/com/example/covid_19_application/MainActivity.java
@@ -1,13 +1,124 @@
 package com.example.covid_19_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Apicall;
+    EditText editplace; //This is the text View to take input from the user abut the Country we want to consider
+    TextView return_call;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editplace = (EditText)findViewById(R.id.editplace);
+        return_call = (TextView)findViewById(R.id.return_call);
+
     }
+
+    public void BuApi(View view) {
+        String s = editplace.getText().toString();
+
+        String url="https://api.covid19api.com/total/country/s";
+        new MyAsyncTaskgetNews().execute(url);
+        Toast.makeText(getApplicationContext(),url,Toast.LENGTH_LONG).show();
+
+    }
+
+
+    // get news from server
+    public class MyAsyncTaskgetNews extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            //before works
+        }
+        @Override
+        protected String  doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            try {
+                String NewsData;
+                //define the url we have to connect with
+                URL url = new URL(params[0]);
+                //make connect with url and send request
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                //waiting for 7000ms for response
+                urlConnection.setConnectTimeout(7000);//set timeout to 5 seconds
+
+                try {
+                    //getting the response data
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    //convert the stream to string
+                    NewsData = ConvertInputToStringNoChange(in);
+                    //send to display data
+                    publishProgress(NewsData);
+                } finally {
+                    //end connection
+                    urlConnection.disconnect();
+                }
+
+            }catch (Exception ex){}
+            return null;
+        }
+        protected void onProgressUpdate(String... progress) {
+
+            try {
+                //display response data
+                Toast.makeText(getApplicationContext(),"Hello boi",Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(),progress[0],Toast.LENGTH_LONG).show();
+                return_call.setText(progress[0]);
+
+            } catch (Exception ex) {
+            }
+
+
+        }
+
+        protected void onPostExecute(String  result2){
+
+
+        }
+
+
+
+
+    }
+
+    // this method convert any stream to string
+    public static String ConvertInputToStringNoChange(InputStream inputStream) {
+
+        BufferedReader bureader=new BufferedReader( new InputStreamReader(inputStream));
+        String line ;
+        String linereultcal="";
+
+        try{
+            while((line=bureader.readLine())!=null) {
+
+                linereultcal+=line;
+
+            }
+            inputStream.close();
+
+
+        }catch (Exception ex){}
+
+        return linereultcal;
+    }
+
+
 }
